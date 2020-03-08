f597b1658d35bc7bd9586c70b7b8a648fce28116
A Reshetnyak <alexreshetnyakmail@gmail.com>
2020-03-08 18:41:06 +0200

    worked on dynamodb setups

--- utils/saveRatingsToDB.js
+++ utils/saveRatingsToDB.js
@@ -1,28 +1,32 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const config = ({
+  "region": "eu-west-1",
+});
 
-module.exports = (yelpData, businessName) => {
-  console.log('Save to DB data', yelpData);
-  const date = JSON.stringify(new Date());
+const dynamoDb = new AWS.DynamoDB(config);
+
+module.exports = async (yelpData, businessName) => {
+  const date = (new Date()).toISOString();
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
-      id: uuid.v1(),
-      businessName: businessName,
-      reviewCount: yelpData.reviewCount,
-      rating: yelpData.rating,
-      scrapedAt: date,
+      id: { S: uuid.v1() },
+      businessName: { S: businessName },
+      reviewCount: { N: yelpData.reviewValue },
+      rating: { N: yelpData.ratingValue },
+      scrapedAt: { S: date },
     },
+    ConditionExpression: 'attribute_not_exists(id)',
   };
 
-  dynamoDb.put(params, err => {
-    if (err) {
-      console.error(`Error saving to DynamoDB : ${JSON.stringify(err)}`);
-      return Promise.reject(`Error saving to DynamoDB : ${JSON.stringify(err)}`);
-    } else {
-      return Promise.resolve(params.Item);
-    }
-  })
+  try {
+    const data = await dynamoDb.putItem(params, err => {
+      if (err) console.log('Error:', err.message);
+    });
+    return params.Item;
+  } catch (error) {
+    throw new Error(`Error saving to DynamoDB : ${error.message}`);
+  }
 };
